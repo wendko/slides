ac5d1d520396dbe3f9d0aa4ffbb84b28dec7103e
wendko <wendko.88@gmail.com>
2019-04-30 11:16:54 +0800

    changes

---
+++ config.js
@@ -0,0 +1,10 @@
+Reveal.initialize({
+    transition: 'none',
+    hash: true,
+    dependencies: [
+        { src: 'plugin/markdown/marked.js' },
+        { src: 'plugin/markdown/markdown.js' },
+        { src: 'plugin/notes/notes.js', async: true },
+        { src: 'plugin/highlight/highlight.js', async: true }
+    ]
+});
