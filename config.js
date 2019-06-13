fe508855bf1051fd84465e5b464f8015c027ec32
wendko <wendko.88@gmail.com>
2019-06-14 03:45:48 +0800

    updated walkthrough

--- config.js
+++ config.js
@@ -2,9 +2,5 @@ Reveal.initialize({
     transition: 'none',
     hash: true,
     dependencies: [
-        { src: 'plugin/markdown/marked.js' },
-        { src: 'plugin/markdown/markdown.js' },
-        { src: 'plugin/notes/notes.js', async: true },
-        { src: 'plugin/highlight/highlight.js', async: true }
     ]
 });
